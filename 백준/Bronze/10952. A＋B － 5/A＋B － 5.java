a9c592839589999697fc214d4ff9efe9444944d4
kimhyunjoong <100519998+jumpjoong@users.noreply.github.com>
2025-04-14 19:05:11 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            if(a == 0 && b == 0) {
+                break;
+            }
+            bw.write((a + b) + "\n");
+        }
+        bw.close();
+    }
+}
